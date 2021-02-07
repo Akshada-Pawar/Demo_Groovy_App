6f60e326c123ae29dd1d21f4af53c55343c29149
Akshada-Pawar <akshadapawar26297@gmail.com>
2021-02-07 19:07:56 +0530

    Add Jenkins File

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-node{
+node('docker'){
         
         stage('Get Latest Code'){
             deleteDir()
@@ -7,6 +7,9 @@ node{
         }
         stage('build'){
                 echo "Building..."
+                docker.image('python:3.5.1').inside{
+                    sh 'python --version'
+                }
                 sh 'python -m py_compile src/app.py'
                 stash(name: 'compiled-results', includes: 'src/*.py*')
                 echo "Build Successful"
@@ -15,7 +18,7 @@ node{
             echo "Testing..."
             def testError = null
             try{
-                sh ''' python test.py '''
+                sh ' python test.py '
             }
             catch(err){
                 testError = err
